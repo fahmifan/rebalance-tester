b94521c99e44eb56e1a0f74f130f880d2f20a417
miun173 <fahmifan173@gmail.com>
2020-02-08 07:39:04 +0700

    feature: add test for load balancer & a web service

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/miun173/rebalance-tester
+
+go 1.12
+
+require (
+	github.com/go-chi/chi v4.0.3+incompatible
+	github.com/sirupsen/logrus v1.4.2
+)
---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	fmt.Println("hello rebalance tester")
+	r := chi.NewRouter()
+	serveWWW(r, "/")
+
+	rebalanceURL := "http://localhost:9000"
+
+	s := service{
+		rebalanceURL: rebalanceURL,
+	}
+	r.Get("/api/sorts", s.handleSorts)
+	port := ":3000"
+
+	log.Infof("start server at %s", port)
+	log.Fatal(http.ListenAndServe(port, r))
+}
+
+type service struct {
+	rebalanceURL string
+}
+
+func (s *service) handleSorts(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get(s.rebalanceURL + "/api/sorts")
+	if err != nil {
+		log.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		w.WriteHeader(resp.StatusCode)
+		w.Write([]byte("request failed"))
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error(err)
+		w.WriteHeader(resp.StatusCode)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	j, err := json.Marshal(map[string]string{"message": string(body)})
+	if err != nil {
+		log.Error(err)
+		w.WriteHeader(resp.StatusCode)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write(j)
+}
+
+func serveWWW(r chi.Router, path string) {
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "www")
+
+	root := http.Dir(filesDir)
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
+}
