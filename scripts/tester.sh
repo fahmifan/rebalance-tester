b960ed1cb5e4e66791e663464f43ce128e51b79e
fahmi.irfan <fahmi.irfan@kumparan.com>
2020-02-29 10:52:53 +0700

    update tester script

--- scripts/tester.sh
+++ scripts/tester.sh
@@ -1,27 +1,78 @@
 #!/bin/bash
-minrate=500
-maxrate=8000
-step=100
-duration=1s
-port=9000
-host=10.113.113.84
-timeout=60s
-pause=5s
-root_folder=lab-aio
-for (( rate=$minrate; rate<$maxrate; rate=${rate+step} ))
-do
-    sleep ${pause}
 
+# port=8000 # web-service
+if [[ -z ${port+x} || "$port" == "" ]]
+    then port=9000
+fi
+
+# host=10.113.113.148 # web service
+if [[ -z ${host+x} || "$host" == "" ]]
+    then host=http://127.0.0.1
+fi
+
+# root_folder=test-folder
+if [[ -z ${root_folder+x} || "$root_folder" == "" ]]
+    then root_folder=test-folder
+fi
+
+# maxcounter=5
+if [[ -z ${maxcounter+x} || "$maxcounter" == "" ]]
+    then maxcounter=5
+fi
+
+# pause=5s
+if [[ -z ${pause+x} || "$pause" == "" ]]
+    then pause=5s
+fi
+
+# timeout=60s
+if [[ -z ${timeout+x} || "$timeout" == "" ]]
+    then timeout=60s
+fi
+
+# duration=1s
+if [[ -z ${duration+x} || "$duration" == "" ]]
+    then duration=1s
+fi
+
+# step=1000
+if [[ -z ${step+x} || "$step" == "" ]]
+    then step=1000
+fi
+
+# maxrate=10000
+if [[ -z ${maxrate+x} || "$maxrate" == "" ]]
+    then maxrate=10000
+fi
+
+# minrate=1000
+if [[ -z ${minrate+x} || "$minrate" == "" ]]
+    then minrate=1000
+fi
+
+echo "==========================="
+echo "target host $host:$port"
+echo "save test file to '$root_folder'"
+echo "test repetition $maxcounter"
+echo "pause between repetition $pause"
+echo "max timeout $timeout"
+echo "attck duration $duration"
+echo "test rate from $minrate to $maxrate with increasing step of $step"
+echo "==========================="
+echo ""
+
+for (( rate=$minrate; rate<$maxrate; rate=$rate+$step ))
+do
     out_folder="${root_folder}/${host}_${rate}rps_${duration}"
     mkdir -p "$out_folder"
 
-    for (( counter=5; counter>0; counter-- ))
+    for (( counter=0; counter<$maxcounter; counter++ ))
     do
-        sleep ${pause}
         out=${out_folder}/${port}_${rate}rps_1s_${counter}.out
-        proxy="GET http://${host}:${port}/api/sorts"
+        proxy="GET ${host}:${port}/api/sorts"
         # vegeta command
         echo ${proxy} | vegeta attack -insecure -duration=${duration} -rate=${rate}/s  -timeout=${timeout} > ${out}
         echo ${out}
+        sleep ${pause}
     done
 done
