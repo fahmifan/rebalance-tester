d20f939f729929057b040737c1bf2b79940f5c58
miun173 <fahmifan173@gmail.com>
2020-03-01 13:13:29 +0700

    add avg success rate

--- scripts/main.py
+++ scripts/main.py
@@ -6,5 +6,8 @@ cases = ["load-balancer", "web-service"]
 for case in cases:
     processing.preprocess(root_dir=f"raw/{case}")
     root_dir = f"json/{case}"
-    data = processing.total_mean_latencies_obj_in_second(root_dir)
-    processing.write_result(f"result/{case}.json", data)
+    latencies = processing.total_mean_latencies_obj_in_second(root_dir)
+    processing.write_result(f"result/latencies_{case}_.json", latencies)
+
+    success_rate = processing.calculate_avg_success_rate("json/load-balancer")
+    processing.write_result(f"result/success_rate_{case}_.json", success_rate)
